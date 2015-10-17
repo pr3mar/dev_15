fca5c0a03a12cfb5ef66ca14caf4d90afeb5104f
Marko Prelevic <markoprelevik@gmail.com>
2015-10-17 10:25:22 +0200

    [pi][dn1] got the structure!!!

--- pi/dn1.py
+++ pi/dn1.py
@@ -40,11 +40,13 @@ class HierarchicalClustering:
         self.data = np.delete(self.data, 38, 1)
         del self.countries[38]
         self.clusters = [[i] for i in range(self.data.shape[1])]
+        self.joining = [[i, 0.0] for i in range(self.data.shape[1])]
 
     def column_distance(self, vector_1, vector_2):
         # print vector_1, vector_2
-        vector = [(x - y) ** 2 for x, y in zip(self.data[:, vector_1], self.data[:, vector_2]) if x >= 0 and y >= 0]
-        return sqrt(sum(vector) / len(vector)) if len(vector) > 0 else 0
+        vector = [(x - y) ** 2 for x, y in zip(self.data[:, vector_1], self.data[:, vector_2])
+                  if x >= 0 and y >= 0]
+        return sqrt(sum(vector) / len(vector)) if len(vector) > 0 else 100
 
     def cluster_distance(self, cluster1, cluster2):
         return self.linkage([self.column_distance(x, y) for x, y in product(cluster1, cluster2)])
@@ -52,17 +54,27 @@ class HierarchicalClustering:
     def closes_clusters(self):
         return min((self.cluster_distance(*c), c) for c in combinations(self.clusters, 2))
 
+    def updateJoints(self, pair, dist):
+        delete = -1
+        set = -1
+        for i in range(len(self.clusters)):
+            # print self.joining[i], pair[0], pair[1]
+            if self.clusters[i] == pair[0]:
+                self.clusters[i] = pair[0] + pair[1]
+                set = i
+            if self.clusters[i] == pair[1]:
+                delete = i
+        self.joining[set] = [self.joining[set]] + [self.joining[delete]] + [dist]
+        del self.clusters[delete]
+        del self.joining[delete]
+
     def run_clustering(self):
-        joining = []
-        tmp = self.clusters
         while len(self.clusters) > 1:  # and i < 2:
             dist, pair = self.closes_clusters()
-            #TODO make lists of tuples!
-            tmp = [x for x in tmp if x not in pair] + [[pair[0]] + [pair[1]]]
-            joining.append(tmp)
-            self.clusters = [x for x in self.clusters if x not in pair] + [pair[0] + pair[1]]
-            # self.printClusters()
-        for i in  joining:
+            # self.clusters = [x for x in self.clusters if x not in pair] + [pair[0] + pair[1]]
+            self.updateJoints(pair, dist)
+            # print self.clusters
+        for i in self.joining:
             print i
 
 # test = HierarchicalClustering("eurovision-final.csv")
