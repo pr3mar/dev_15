ba3747f1b0412955c9367c779312f9e8c89fc82b
Marko Prelevic <markoprelevik@gmail.com>
2015-10-19 10:37:49 +0200

    [uz] dn1 work

--- uz/exercise1/dn1_ex2.m
+++ uz/exercise1/dn1_ex2.m
@@ -1,9 +1,43 @@
+% (a) --> manual treshold (65)
 A = rgb2gray(imread('bird.jpg'));
-figure (3); imagesc(imread('bird.jpg')); 
-r = myhist(A, 10);
+treshold = 65;
+M = A > treshold;
+figure(1) ; imagesc(M); colormap gray
+% figure ; imagesc(imread('bird.jpg')); 
+
+
+% (b) --> histogram (provided code)
+[H, bins] = myhist(A, 10);
 % c = hist(double(A), 10);
-figure ; bar(r);
+% figure(2) ; bar(bins, H);
 % figure ; bar(c);
 
-M = A > 65;
-figure ; imagesc(M); colormap gray
+% (c) --> embedded histogram function
+I = double(rgb2gray(imread('umbrellas.jpg')));
+P = I(:); % 2d to 1d vector
+% figure(1); clf;
+
+bins = 10;
+H = hist(P, bins);
+H = H/sum(H); % normalize
+% subplot(1,3,1); bar(H, 'b');
+
+bins = 20;
+H = hist(P, bins);
+H = H/sum(H); % normalize
+% subplot(1,3,2); bar(H, 'b');
+
+bins = 30;
+H = hist(P, bins);
+H = H/sum(H); % normalize
+% subplot(1,3,3); bar(H, 'b');
+
+% (d) --> other images (?)
+
+% (e) --> otsu's threshold
+bird = double(rgb2gray(imread('bird.jpg')));
+threshold = threshold_otsu(bird);
+O = bird > threshold;
+figure ; imagesc(O); colormap gray
+
+
