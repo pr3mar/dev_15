2ab9421eafc00dd63e33f457f5101ab60ab9c4a1
Marko Prelevic <markoprelevik@gmail.com>
2015-10-21 09:04:08 +0200

    [uz] after defence

--- uz/exercise1/up.m
+++ uz/exercise1/up.m
@@ -8,47 +8,47 @@ up_mask = imerode(imdilate(up_mask,up_se),up_se);
 figure(1) ; imagesc(up_mask); colormap gray
 up_masked = immask(up_rgb, up_mask);
 figure(1) ; imagesc(up_masked);
-up_array = up_rgb(:);
-[H, bins] = myhist(up_rgb, 10);
-c = hist(double(up_rgb), 10);
-figure(5); clf ; subplot(1,2,1); bar(H, bins);
-subplot(1,2,2); bar(c);
+% up_array = up_rgb(:);
+% [H, bins] = myhist(up_rgb, 10);
+% c = hist(double(up_rgb), 10);
+% figure(5); clf ; subplot(1,2,1); bar(H, bins);
+% subplot(1,2,2); bar(c);
 
 
-stones = imread('stones.jpg');
-stones_gray = rgb2gray(stones);
-th = multithresh(stones_gray, 4);
-stones_mask = not(stones_gray < th(1));
-b_se = strel('disk', 4);
-stones_mask = imdilate(imerode(stones_mask, b_se), b_se);
-% figure(2) ; imagesc(stones_mask); colormap gray
-
-labels = bwlabel(stones_mask);
-
-label_max = max(labels(:));
-maxSum = 1000000; % minSum = intmax
-maxI = 0;   % minI = 0
-sums = zeros(1, label_max);
-indices = (1:label_max);
-% remove biggest portion / smallest 
-for i = 1: label_max
-    suma = sum(labels(:) == i);
-    sums(i) = suma;
-%     if suma < maxSum % if suma < maxSum
-%         labels(labels == i) = 0;
-% %         maxSum = suma;
-% %         maxI = i; % minI = i
-%     end;
-end;
-% labels(labels == maxI) = 255;
-indices(sums > mean(sums)) = 0;
-for i = 1 : length(indices)
-    labels(labels(:) == indices(i)) = 0;    
-end
-
-% rnd = randi([0,label_max])
-% labels(labels == rnd) = 0;
-
-figure(3) ; imagesc(labels);
-labels(labels > 1) = 1;
-figure(4) ; imagesc(immask(stones, labels));
+% stones = imread('stones.jpg');
+% stones_gray = rgb2gray(stones);
+% th = multithresh(stones_gray, 4);
+% stones_mask = not(stones_gray < th(1));
+% b_se = strel('disk', 4);
+% stones_mask = imdilate(imerode(stones_mask, b_se), b_se);
+% % figure(2) ; imagesc(stones_mask); colormap gray
+% 
+% labels = bwlabel(stones_mask);
+% 
+% label_max = max(labels(:));
+% maxSum = 1000000; % minSum = intmax
+% maxI = 0;   % minI = 0
+% sums = zeros(1, label_max);
+% indices = (1:label_max);
+% % remove biggest portion / smallest 
+% for i = 1: label_max
+%     suma = sum(labels(:) == i);
+%     sums(i) = suma;
+% %     if suma < maxSum % if suma < maxSum
+% %         labels(labels == i) = 0;
+% % %         maxSum = suma;
+% % %         maxI = i; % minI = i
+% %     end;
+% end;
+% % labels(labels == maxI) = 255;
+% indices(sums > (mean(sums) + std(sums)*3)) = 0;
+% for i = 1 : length(indices)
+%     labels(labels(:) == indices(i)) = 0;    
+% end
+% 
+% % rnd = randi([0,label_max])
+% % labels(labels == rnd) = 0;
+% 
+% figure(3); clf ; imagesc(labels);
+% labels(labels > 1) = 1;
+% figure(4) ; clf; imagesc(immask(stones, labels));
