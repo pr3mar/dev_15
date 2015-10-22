75a9002ebf0b021436a528842ec770ea210be15f
Marko Prelevic <markoprelevik@gmail.com>
2015-10-22 15:28:33 +0200

    [rgti] homework 1 stuff

--- rgti/dn1/scripts/coreFunction.js
+++ rgti/dn1/scripts/coreFunction.js
@@ -2,6 +2,8 @@
  * Created by Power User on 22.10.2015.
  */
 var fileContent = "[nan]";
+var vertices = [];
+var triangles = [];
 
 function handleFiles(event) {
     var file = event.target.files[0];
@@ -9,7 +11,7 @@ function handleFiles(event) {
     if(file.type.match(/text.*/)) {
         var reader = new FileReader();
         reader.onloadend = function() {
-            document.getElementById("fileDisplayArea").innerHTML = reader.result;
+            //document.getElementById("fileDisplayArea").innerHTML = reader.result;
             fileContent = reader.result;
             startWorking();
         };
@@ -20,12 +22,27 @@ window.onload = function() {
     document.getElementById("fileChooser").addEventListener("change", handleFiles, false);
 };
 
+function parseElements(line) {
+    var identifier = line.shift();
+    switch(line.shift()) {
+        case "v":
+            vertices.push(line);
+            break;
+        case "f":
+            triangles.push(line);
+            break;
+        default :
+            console.log("error");
+    }
+}
+
 function parse(fileContent) {
     console.log("parsing");
     lines = fileContent.split("\n");
-    for(x in lines) {
-        console.log(x, lines[x]);
-    }
+    lines.forEach(function (x) {
+        if(x.length > 1)
+            parseElements(x.split(" "));
+    });
     result = fileContent;
     return result;
 }
@@ -33,5 +50,6 @@ function parse(fileContent) {
 function startWorking() {
     console.log("started working!!!");
     result = parse(fileContent);
-    console.log(result);
+    console.log(vertices);
+    console.log(triangles);
 }
