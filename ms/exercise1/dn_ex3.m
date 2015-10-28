3cb16bddcee1c8c5b852ae6cc76132fd41600fba
Marko Prelevic <markoprelevik@gmail.com>
2015-10-28 22:05:50 +0100

    [uz] ex2

--- ms/exercise1/dn_ex3.m
+++ ms/exercise1/dn_ex3.m
@@ -10,26 +10,26 @@
 % subplot(2, 4, 7); imshow(trucks_hsv(:,:,2)); title('saturation');
 % subplot(2, 4, 8); imshow(trucks_hsv(:,:,3)); title('value');
 
-% % (b) --> change channels according to f
-% trucks_rgb = imread('trucks.jpg');
-% trucks_hsv = rgb2hsv(trucks_rgb);
-% figure(2); clf;
-% x = 1;
-% for i = 1:3
-%     for j = 1:10
-%         f = (j - 1) / 9;
-%         subplot(6, 10, x);
-%         tmp = trucks_rgb;
-%         tmp(:,:,i) = 255 .* f;
-%         imshow(tmp);
-%         tmp = trucks_hsv;
-%         subplot(6, 10, x + 30);
-%         tmp(:,:,i) = f;
-%         imshow(hsv2rgb(tmp));
-%         x = x + 1;
-%     end;
-% %     x = x + 10;x
-% end;
+% (b) --> change channels according to f
+trucks_rgb = imread('trucks.jpg');
+trucks_hsv = rgb2hsv(trucks_rgb);
+figure(2); clf;
+x = 1;
+for i = 1:3
+    for j = 1:10
+        f = (j - 1) / 9;
+        subplot(6, 10, x);
+        tmp = trucks_rgb;
+        tmp(:,:,i) = 255 .* f;
+        imshow(tmp);
+        tmp = trucks_hsv;
+        subplot(6, 10, x + 30);
+        tmp(:,:,i) = f;
+        imshow(hsv2rgb(tmp));
+        x = x + 1;
+    end;
+%     x = x + 10;x
+end;
 
 % (c) --> different color spaces
 trucks = imread('trucks.jpg');
@@ -51,9 +51,9 @@ trucks_hsv = rgb2hsv(imread('trucks.jpg'));
 blue_hue = trucks_hsv(:,:,1) > 0.55 & trucks_hsv(:,:,1) <= 0.73;
 subplot(1,4,4); imshow(blue_hue); title('hue treshold');
 
-I = ones (20 , 255 , 3) ;
-I (: , : , 1) = ones (20 , 1) * linspace (0 , 1 , 255) ;
-figure; image ([0 , 1] , [0 , 1] , hsv2rgb (I) ) ;
+% I = ones (20 , 255 , 3) ;
+% I (: , : , 1) = ones (20 , 1) * linspace (0 , 1 , 255) ;
+% figure; image ([0 , 1] , [0 , 1] , hsv2rgb (I) ) ;
 
 % I1 = imread('http://i.stack.imgur.com/1KyJA.jpg');
 % I2=I1;
