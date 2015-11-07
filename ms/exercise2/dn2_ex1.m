9b99704eb518026379b6c1e939c8def9c7757ace
Marko Prelevic <markoprelevik@gmail.com>
2015-11-07 13:18:47 +0100

    [uz] dn2 progress

---
+++ ms/exercise2/dn2_ex1.m
@@ -0,0 +1,22 @@
+% % (b)
+% 
+% video = read_video('shaky');
+% vf = figure(1);
+% % for i = 1:size(video,4)
+% %     set(0, 'CurrentFigure', vf);
+% %     imshow(video(:,:,:,i));
+% %     pause(0.05);
+% % end
+% % clf;
+% 
+% % difference between frames
+% for i = 1:size(video,4) - 1
+%     set(0, 'CurrentFigure', vf);
+%     imshowpair(video(:,:,:,i), video(:,:,:,i + 1), 'diff');
+%     pause(0.05);
+% end
+% clf;
+% % use darker areas because tehre is little or no difference between frames
+
+% (c)
+
