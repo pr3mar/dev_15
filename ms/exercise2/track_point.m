c5aaeaadf1642b59b5b8884c248cefe73d71922f
Marko Prelevic <markoprelevik@gmail.com>
2015-11-11 00:57:41 +0100

    [ms] some progress with ex1

--- ms/exercise2/track_point.m
+++ ms/exercise2/track_point.m
@@ -14,20 +14,23 @@ function [ trackX, trackY ] = track_point( frames, startX, startY, patchSize, se
     for i = 2:n
         centerX = trackX(i - 1); centerY = trackY(i - 1);
         currentPatch = rgb2gray(frames(:,:,:,i));
-        x1 = max(1, centerX - halfSize(searchSize));
-        y1 = max(1, centerY - halfSize(searchSize));
-        x2 = min(width, centerX + halfSize(searchSize) - 1);
-        y2 = min(height, centerY + halfSize(searchSize) - 1);
+        x1 = max(1, centerX - halfSize(searchSize))
+        y1 = max(1, centerY - halfSize(searchSize))
+        x2 = min(width, centerX + halfSize(searchSize) - 1)
+        y2 = min(height, centerY + halfSize(searchSize) - 1)
         currentPatch = currentPatch(y1:y2, x1:x2);
         currSize = size(currentPatch)
         tt = normxcorr2(template, currentPatch); % dokumentacija
 %         tt_size = size(tt);
-        [x] = max(max(tt));
-        [indY, indX] = find(tt == max(tt(:)), 1)
+        [~, ind] = max(max(tt));
+        [indX, indY] = ind2sub(size(tt),ind);
         % how to do this????
-        trackX(i) = indX + x1 - size(template,1)/2;
-        trackY(i) = indY + y1 - size(template,2)/2;
+        trackX(i) = indX - size(template,1) + x1;
+        trackY(i) = indY - size(template,2) + y1;
         % iste slike zaporedoma
+        if( i == 3)
+            break
+        end
     end
 end
 
