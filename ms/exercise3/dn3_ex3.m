5c7d9eace9a15ed76639b50ec1247f7f09d854c3
Marko Prelevic <markoprelevik@gmail.com>
2015-11-24 19:27:18 +0100

    [ms] dn3 ex3 done.

--- ms/exercise3/dn3_ex3.m
+++ ms/exercise3/dn3_ex3.m
@@ -21,34 +21,38 @@
 %     axis tight; axis equal;
 % end
 
-sea = rgb2gray(imread('underwater.jpg'));
-eagle = rgb2gray(imread('eagle.jpg'));
+% sea = rgb2gray(imread('underwater.jpg'));
+% eagle = rgb2gray(imread('eagle.jpg'));
+% eagle_p = laplace_pyramid(eagle, level);
+% sea_p = laplace_pyramid(sea, level);
 mask = rgb2gray(imread('eagle_mask.png'));
 level = 4;
-eagle_p = laplace_pyramid(eagle, level);
-sea_p = laplace_pyramid(sea, level);
-% eagle_p = cell(3,1);
-% sea_p = cell(3,1);
-% for i = 1:3
-%     eagle_p{i} = laplace_pyramid(eagle(:,:,i), level);
-%     sea_p{i} = laplace_pyramid(sea(:,:,i), level);
-% end
+sea = imread('underwater.jpg');
+eagle = imread('eagle.jpg');
+eagle_p = cell(3,1);
+sea_p = cell(3,1);
+for i = 1:3
+    eagle_p{i} = laplace_pyramid(eagle(:,:,i), level);
+    sea_p{i} = laplace_pyramid(sea(:,:,i), level);
+end
 mask_p = gauss_pyramid(mask, level);
 result_p = sea_p;
 % figure(3); colormap gray;
 result = zeros(size(eagle));
-for i = 1:level
-    [iter_y, iter_x] = find(mask_p{i} ~= 0);
-    mask_p{i} = double(mask_p{i}) / max(mask_p{i}(:));
-    for j = 1:numel(iter_y)
-%         result_p{k}{i}(iter_y(j), iter_x(j)) = ...
-%              mask_p{i}(iter_y(j), iter_x(j)) * eagle_p{k}{i}(iter_y(j), iter_x(j)) + ...
-%              (1 - mask_p{i}(iter_y(j), iter_x(j))) * sea_p{k}{i}(iter_y(j), iter_x(j));
-        result_p{i}(iter_y(j), iter_x(j)) = ...
-              mask_p{i}(iter_y(j), iter_x(j)) * eagle_p{i}(iter_y(j), iter_x(j)) + ...
-              (1 - mask_p{i}(iter_y(j), iter_x(j))) * sea_p{i}(iter_y(j), iter_x(j));
+for k = 1:3
+    for i = 1:level
+        [iter_y, iter_x] = find(mask_p{i} ~= 0);
+        mask_p{i} = double(mask_p{i}) / max(mask_p{i}(:));
+        for j = 1:numel(iter_y)
+    %         result_p{k}{i}(iter_y(j), iter_x(j)) = ...
+    %              mask_p{i}(iter_y(j), iter_x(j)) * eagle_p{k}{i}(iter_y(j), iter_x(j)) + ...
+    %              (1 - mask_p{i}(iter_y(j), iter_x(j))) * sea_p{k}{i}(iter_y(j), iter_x(j));
+            result_p{k}{i}(iter_y(j), iter_x(j)) = ...
+                  mask_p{i}(iter_y(j), iter_x(j)) * eagle_p{k}{i}(iter_y(j), iter_x(j)) + ...
+                  (1 - mask_p{i}(iter_y(j), iter_x(j))) * sea_p{k}{i}(iter_y(j), iter_x(j));
 
+        end
+        result(:,:,k) = result(:,:,k) + result_p{k}{i};
     end
-    result = result + result_p{i};
 end
-figure(4); imagesc(result); colormap gray;
+figure(4); imagesc(uint8(result)); colormap gray;
