6eaf243579b1017774d1ca719a9cbafc338b33ec
Marko Prelevic <markoprelevik@gmail.com>
2015-12-10 00:11:29 +0100

    [ms] dn4 little more details??

--- ms/exercise4/dn4_ex3.m
+++ ms/exercise4/dn4_ex3.m
@@ -2,17 +2,21 @@ addpath('grabcut');
 
 % P = rand(100,100,2); % initializesomerandomdata
 % P(:,:,2) = 1 - P(:,:,1);
+% figure(1); imagesc(P(:,:,1));
+% figure(2); imagesc(P(:,:,2));
 % cost = (ones(2) - eye(2))*10; % define cost(experiment with scaling)
 % gch = GraphCut('open',P, cost);% initialize GraphCut context : P is probability matrix
 % [gch, L] = GraphCut('expand', gch);% perform graph?cut and retreive result
 % gch = GraphCut('close', gch);% release memory
 % S = L ~= 0;
+% figure(3); imagesc(S);
 
-img = imread('parrot2.jpg');
+img = imread('parrot.jpg');
 % figure(1); clf; imagesc(img);
 % mask = roipoly();
+% figure(1); imagesc(mask); colormap gray;
 res = grab_cut(img, mask, 20);
 
 figure(2); colormap gray;
-% imagesc(res);
-imagesc(immask(img, res));
+imagesc(res);
+% imagesc(immask(img, res));
--- ms/exercise4/grab_cut.m
+++ ms/exercise4/grab_cut.m
@@ -3,8 +3,31 @@ function [ img ] = grab_cut( I, mask, iters )
     img = reshape(I, h * w,c);
     back = zeros(h * w, c);
     frg = zeros(h * w, c);
+%     I = reshape(I, h*w, 3);
     for k = 1:iters
         mask = reshape(mask, h * w, 1);
+%         S = reshape(S, h*w, 1);
+%         A(:,1) = double(I(:,1)) .* double(~S);
+%         A(:,2) = double(I(:,2)) .* double(~S);
+%         A(:,3) = double(I(:,3)) .* double(~S);
+% 
+%         %background
+%         B(:,1) = double(I(:,1)) .* double(S);
+%         B(:,2) = double(I(:,2)) .* double(S);
+%         B(:,3) = double(I(:,3)) .* double(S);
+% 
+%         mA = mean(A);
+%         mB = mean(B);
+% 
+%         covA = cov(A);
+%         covB = cov(B);
+% 
+%         pxf = gaussPDF(A', mA', covA);
+%         pxb = gaussPDF(B', mB', covB);
+% 
+%         pfx = pxf ./ (pxf + pxb);
+%         pfx = reshape(pfx,h,w); 
+%         pbx = 1 - pfx;
         for i = 1:c
 %             size(back(:,i)), size(img(:,i)), size(mask)
             back(:,i) = double(img(:,i)) .* double(~mask);
@@ -15,16 +38,18 @@ function [ img ] = grab_cut( I, mask, iters )
         prob_b = gaussPDF(back', mean_b', cov_b);
         prob_f = gaussPDF(frg', mean_f', cov_f);
         p = prob_f ./ (prob_f + prob_b);
-        p = reshape(p, h, w, 1);
-        p(:,:,2) = 1 - p(:,:,1);
-        
-        cost = (ones(2) - eye(2))*2;
+        f = reshape(p, h, w, 1);
+        b = 1 - f;
+        p = cat(3, b, f);
+%         p(:,:,1) = 1 - p(:,:,1);
+%         p(:,:,2) = 1 - p(:,:,1);
+%         p = cat(3, pfx, pbx);
+        cost = (ones(2) - eye(2))*0.001;
         gch = GraphCut('open', p, cost);
         [gch, L] = GraphCut('expand', gch);
         gch = GraphCut('close', gch);
         mask = L ~= 0;
     end
-    
     img = mask;
 end
 
