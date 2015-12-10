d5c074002475ffe4b2bebd93a4b86ac0826266cf
Marko Prelevic <markoprelevik@gmail.com>
2015-12-10 14:57:46 +0100

    [ms] dn4 completed.

--- ms/exercise4/dn4_ex3.m
+++ ms/exercise4/dn4_ex3.m
@@ -11,12 +11,15 @@ addpath('grabcut');
 % S = L ~= 0;
 % figure(3); imagesc(S);
 
-img = imread('parrot.jpg');
+img = imread('parrot2.jpg');
+mask = imread('parrot2_mask.png');
 % figure(1); clf; imagesc(img);
 % mask = roipoly();
 % figure(1); imagesc(mask); colormap gray;
-res = grab_cut(img, mask, 20);
-
+% res = grab_cut(img, mask, 17, 0.03, 2); % parrot 1
+res = grab_cut(img, mask, 25, 0.001, 2); % parrot 1
 figure(2); colormap gray;
-imagesc(res);
+imagesc(immask(img, res));
+% res = grab_cut_metod(img, mask);
+% figure(3); colormap gray;
 % imagesc(immask(img, res));
--- ms/exercise4/grab_cut.m
+++ ms/exercise4/grab_cut.m
@@ -1,62 +1,46 @@
-function [ img ] = grab_cut( I, mask, iters )
+function [ out ] = grab_cut( I, mask, iters, alfa, sigma )
+    clc
     [h, w, c] = size(I);
-    img = reshape(I, h * w,c);
-    back = zeros(h * w, c);
-    frg = zeros(h * w, c);
-%     I = reshape(I, h*w, 3);
-    for k = 1:iters
-        mask = reshape(mask, h * w, 1);
-%         S = reshape(S, h*w, 1);
-%         A(:,1) = double(I(:,1)) .* double(~S);
-%         A(:,2) = double(I(:,2)) .* double(~S);
-%         A(:,3) = double(I(:,3)) .* double(~S);
-% 
-%         %background
-%         B(:,1) = double(I(:,1)) .* double(S);
-%         B(:,2) = double(I(:,2)) .* double(S);
-%         B(:,3) = double(I(:,3)) .* double(S);
-% 
-%         mA = mean(A);
-%         mB = mean(B);
-% 
-%         covA = cov(A);
-%         covB = cov(B);
-% 
-%         pxf = gaussPDF(A', mA', covA);
-%         pxb = gaussPDF(B', mB', covB);
-% 
-%         pfx = pxf ./ (pxf + pxb);
-%         pfx = reshape(pfx,h,w); 
-%         pbx = 1 - pfx;
-        for i = 1:c
-%             size(back(:,i)), size(img(:,i)), size(mask)
-            back(:,i) = double(img(:,i)) .* double(~mask);
-            frg(:,i) = double(img(:,i)) .* double(mask);
+    img = reshape(I, h * w, c);
+    foreground = zeros(h*w, c);
+    back = zeros(h*w, c);
+    for i = 1:iters
+        mask = reshape(mask, h*w, 1);
+        for j = 1:c
+            foreground(:,j) = double(img(:,j)) .* double(mask);
+            back(:,j) = double(img(:,j)) .* double(~mask);
         end
-        mean_b = mean(back); cov_b = cov(back);
-        mean_f = mean(frg); cov_f = cov(frg);
-        prob_b = gaussPDF(back', mean_b', cov_b);
-        prob_f = gaussPDF(frg', mean_f', cov_f);
-        p = prob_f ./ (prob_f + prob_b);
-        f = reshape(p, h, w, 1);
-        b = 1 - f;
-        p = cat(3, b, f);
-%         p(:,:,1) = 1 - p(:,:,1);
-%         p(:,:,2) = 1 - p(:,:,1);
-%         p = cat(3, pfx, pbx);
-        cost = (ones(2) - eye(2))*0.001;
+        
+        mean_f = mean(double(foreground)); cov_f = cov(double(foreground));
+        mean_b = mean(double(back)); cov_b = cov(double(back));
+        
+        pxf = gaussPDF(double(img)', mean_f', cov_f);
+        pxb = gaussPDF(double(img)', mean_b', cov_b);
+        
+        pfx = pxf ./ (pxf + pxb);
+        pfx = reshape(pfx, h, w);
+        pbx = 1 - pfx;
+        p = cat(3, pfx, pbx);
+%         figure(1); imagesc(p(:,:,1)); title('pfx');
+%         figure(2); imagesc(p(:,:,2)); title('pbx');
+        cost = (ones(2) - eye(2)) * alfa;
         gch = GraphCut('open', p, cost);
-        [gch, L] = GraphCut('expand', gch);
+        [gch, L] =  GraphCut('expand', gch);
         gch = GraphCut('close', gch);
-        mask = L ~= 0;
+%         figure(1); imagesc(L); title('L');
+        mask = (L ~= 0);
     end
-    img = mask;
+%     out = mask;
+%     figure(3); clf; imagesc(bwlabel(mask));
+    [label, num] = bwlabel(mask);
+    mask = zeros(size(mask));
+    tsh = 1500;
+    for i = 1:num
+        tmp = sum(label(:) == i);
+        if (tmp > tsh)
+            mask = or(mask, (label == i));
+        end
+    end
+    out = imgaussfilt(double(mask), sigma);
 end
 
-
-%     figure(1);clf;imagesc(uint8(back));
-%     figure(2);clf;imagesc(uint8(frg));
-
-%     back = reshape(back, h*w, c);
-%     frg = reshape(frg, h*w, c);
-%     mask = reshape(mask, h*w,1);
