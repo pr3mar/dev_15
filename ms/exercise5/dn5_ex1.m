c385e83812255f6b5b24670459f5540721777a2a
Marko Prelevic <markoprelevik@gmail.com>
2015-12-15 20:35:14 +0100

    [ms] dn5 ex1 done.

--- ms/exercise5/dn5_ex1.m
+++ ms/exercise5/dn5_ex1.m
@@ -1,6 +1,14 @@
-% video = read_video('bigbuck');
+video = read_video('bigbuck');
+% view the video
+vf = figure(1);
+for i = 1:frames
+    set(0, 'CurrentFigure', vf);
+    imshow(video(:,:,:,i));
+    pause(0.005);
+end
+
 bins = 8;
-[h, w, c, frames] = size(video);
+% [h, w, c, frames] = size(video);
 histograms = zeros(bins, (3*size(video,2)), size(video,4));
 % size(histograms)
 for i = 1:frames
@@ -8,9 +16,22 @@ for i = 1:frames
 end
 
 dists = zeros(size(video,4), 1);
-
+dist_type = 'hellinger';
 for i = 1:(frames - 1)
-    dists(i) = distance(histograms(:,:,i), histograms(:,:,i + 1), 'chi2');
+    dists(i) = distance(histograms(:,:,i), histograms(:,:,i + 1), dist_type);
 end
 
-plot(dists);
+figure(1); plot(dists); title(sprintf('distance = ''%s'', bins = %d', dist_type, bins));
+
+[max_val, max_ind] = max(dists);
+tsh = max_val * 0.3;
+[results] = find(dists > tsh);
+count = 1;
+for i = 1:numel(results)
+    start = (results(i) - 2);
+    finish = (results(i) + 2);
+    for j = start:finish
+         subplot(numel(results), 5, count); imshow(video(:,:,:,j));
+        count = count + 1;
+    end
+end
